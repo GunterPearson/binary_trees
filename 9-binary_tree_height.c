13a0ca1db5b3b6e28b496be837ba2e84583acb29
GunterPearson <1715@holbertonschool.com>
2020-07-23 17:56:19 +0000

    fixed problems with 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
-	if (tree == NULL)
-		return (0);
-
 	if (tree)
 	{
-		left = 1 + binary_tree_height(tree->left);
-		right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		else
+			left = 0;
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		else
+			right = 0;
 		if (left > right)
 			return (left);
 		else
 			return (right);
 	}
-	return (-1);
+	return (0);
 }
