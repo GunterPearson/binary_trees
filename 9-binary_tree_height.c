bcf9c3b4bebf5045b72bb62788518a989fd10328
GunterPearson <1715@holbertonschool.com>
2020-07-23 17:46:32 +0000

    fixed bugs for 9 and 10

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree)
 	{
 		left = 1 + binary_tree_height(tree->left);
