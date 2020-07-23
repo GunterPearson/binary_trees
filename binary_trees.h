e8be171f8cd8b230eee5155f7f26848b591c5458
GunterPearson <1715@holbertonschool.com>
2020-07-23 17:06:48 +0000

    pushing file 7 and 8 along with header file

--- binary_trees.h
+++ binary_trees.h
@@ -51,4 +51,10 @@ int binary_tree_is_root(const binary_tree_t *node);
 /* FILE 6 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
+/* FILE 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* FILE 8 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif
